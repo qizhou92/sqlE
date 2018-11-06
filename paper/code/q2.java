2f9fc2d14ef9f5d7f62e71d4bd0b8b80f5063de6
“周齐” <zq166665@alibaba-inc.com>
2018-11-06 15:47:34 -0500

    initial paper draft

---
+++ paper/code/q2.java
@@ -0,0 +1,8 @@
+p0 (int EMPNO, String ENAME, int DEPTNO){
+  if (DEPTNO == 10){
+    if(DEPTNO+5> DEPTNO){
+      return (EMPNO,ENAME,DEPTNO)}}}
+p1 (int EMPNO, String ENAME, int DEPTNO){
+  if (DEPTNO == 10){
+    if(15> DEPTNO){
+      return (EMPNO,ENAME,DEPTNO)}}}
