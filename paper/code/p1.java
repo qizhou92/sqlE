1f31b49145e6e6149a85c01b87b83c171e50e337
“周齐” <zq166665@alibaba-inc.com>
2018-11-28 21:57:29 -0500

    re-write the overview

--- paper/code/p1.java
+++ paper/code/p1.java
@@ -1,8 +1,8 @@
 p0 (int EMPNO, String ENAME, int DEPTNO){
   if (DEPTNO == 10){
-    if(DEPTNO+5> DEPTNO){
+    if(DEPTNO+5> EMPNO){
       return (EMPNO,ENAME,DEPTNO)}}}
 p1 (int EMPNO, String ENAME, int DEPTNO){
   if (DEPTNO == 10){
-    if(15> DEPTNO){
+    if(15> EMPNO){
       return (EMPNO,ENAME,DEPTNO)}}}
--- paper/code/q1.sql
+++ paper/code/q1.sql
@@ -1,13 +1,9 @@
 TABLE EMP(EMPNO int, ENAME String, DEPTNO int);
 
-q0: SELECT * FROM 
+q1: SELECT * FROM 
      (SELECT * FROM EMP WHERE DEPTNO = 10) AS t 
     WHERE t.DEPTNO + 5 > t.EMPNO;
- 
-Filter (DEPTNO + 5 > EMPNO) (Filter DEPTNO = 10 (Table EMP))
 
-q1: SELECT * FROM 
+q2: SELECT * FROM 
      (SELECT * FROM EMP WHERE DEPTNO = 10) AS t1 
     WHERE 15 > t1.EMPNO; 
-
-Filter (15 > EMPNO) (Filter DEPTNO = 10 (Table EMP))
